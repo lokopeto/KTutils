2ea25d22a60a2507e3389c732dfabea5027ab645
lokopeto <lokopeto02@gmail.com>
2025-02-26 17:43:52 -0300

    Update KTutils.lua
    fixes and easydebug addition

--- KTutils/KTutils.lua
+++ KTutils/KTutils.lua
@@ -15,7 +15,19 @@ end
 
 -- code from https://defold.com/manuals/camera/#converting-mouse-to-world-coordinates
 -- may changes occur to the code on the future
-local function utils.screen_to_world(sx, sy, sz, window_width, window_height, projection, view)
+
+--- Convert from screen to world coordinates
+-- @param sx Screen x
+-- @param sy Screen y
+-- @param sz Screen z
+-- @param window_width Width of the window (use render.get_width() or window.get_size().x)
+-- @param window_height Height of the window (use render.get_height() or window.get_size().y)
+-- @param projection Camera/render projection (use go.get("#camera", "projection"))
+-- @param view Camera/render view (use go.get("#camera", "view"))
+-- @return wx World x
+-- @return wy World y
+-- @return wz World z
+function utils.screen_to_world(sx, sy, sz, window_width, window_height, projection, view)
 	local inv = vmath.inv(projection * view)
 	sx = (2 * sx / window_width) - 1
 	sy = (2 * sy / window_height) - 1
@@ -24,6 +36,12 @@ local function utils.screen_to_world(sx, sy, sz, window_width, window_height, pr
 	local wy = sx * inv.m10 + sy * inv.m11 + sz * inv.m12 + inv.m13
 	local wz = sx * inv.m20 + sy * inv.m21 + sz * inv.m22 + inv.m23
 	return wx, wy, wz
+end
+
+-- debug with drawning text on the screen
+-- using render draw text
+function utils.easydebug(x, y, string)
+	msg.post("@render:", "draw_text", { text = string, position = vmath.vector3(x, y, 0) })
 end
 
 return utils
