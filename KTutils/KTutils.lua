4f95b09dec039435a8d63feff31fd3fab334e833
lokopeto <34287720+lokopeto@users.noreply.github.com>
2025-03-28 14:18:07 -0300

    added ip concat

--- KTutils/KTutils.lua
+++ KTutils/KTutils.lua
@@ -1,9 +1,9 @@
-local M = {}
+local utils = {}
 
 local floor_limit = 10000
 
--- float imprecise handler
-function M.zeroout(num)
+-- simple float imprecise handler
+function utils.zeroout(num)
 	if math.floor(num * floor_limit) == -1 then
 		return 0
 	elseif math.floor(num * floor_limit) == 1 then
@@ -12,5 +12,48 @@ function M.zeroout(num)
 		return num
 	end
 end
+--
 
-return M
+-- code from https://defold.com/manuals/camera/#converting-mouse-to-world-coordinates
+-- may changes occur to the code on the future
+
+--- Convert from screen to world coordinates
+-- @param sx Screen x
+-- @param sy Screen y
+-- @param sz Screen z
+-- @param window_width Width of the window (use render.get_width() or window.get_size().x)
+-- @param window_height Height of the window (use render.get_height() or window.get_size().y)
+-- @param projection Camera/render projection (use go.get("#camera", "projection"))
+-- @param view Camera/render view (use go.get("#camera", "view"))
+-- @return wx World x
+-- @return wy World y
+-- @return wz World z
+function utils.screen_to_world(sx, sy, sz, window_width, window_height, projection, view)
+	local inv = vmath.inv(projection * view)
+	sx = (2 * sx / window_width) - 1
+	sy = (2 * sy / window_height) - 1
+	sz = (2 * sz) - 1
+	local wx = sx * inv.m00 + sy * inv.m01 + sz * inv.m02 + inv.m03
+	local wy = sx * inv.m10 + sy * inv.m11 + sz * inv.m12 + inv.m13
+	local wz = sx * inv.m20 + sy * inv.m21 + sz * inv.m22 + inv.m23
+	return wx, wy, wz
+end
+
+-- debug with drawning text on the screen
+function utils.easydebug(x, y, string)
+	msg.post("@render:", "draw_text", { text = string, position = vmath.vector3(x, y, 0) })
+end
+--
+
+-- convenient functions for ips
+function utils.concat_ip(ip,port)
+	return tostring(ip) .. ":" .. tostring(port) -- 127.0.0.0:0000
+end
+
+function utils.deconcat_ip(ip)
+	local loc = string.find(ip, ":")
+	return string.sub(ip,0,loc - 1), string.sub(ip,loc + 1) -- ip, port
+end
+--
+
+return utils
